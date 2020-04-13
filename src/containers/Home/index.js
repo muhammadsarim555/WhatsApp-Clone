d11cc7b326ab94932f1b6e9a1439d9f42df7d6d0
Ahaseeb98 <ahaseeb12251998@gmail.com>
2020-04-13 19:01:52 +0500

    list contact dynamic

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -18,75 +18,97 @@ import {CustomComponents} from '../../components';
 import axios from 'react-native-axios';
 import {API_URL} from '../../config/apiConfig';
 
-function Home({navigation}) {
-  const [allMobileUsers, setAllMobileUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-console.log("contact list")
-  function fetchData(contacts) {
-    // let final = [];
-    contacts.map((v, i) => {
-      axios
-        .get(
-          `http://${API_URL}:8000/user/login?contact_no=${v.phoneNumbers
-            .length !== 0 && v.phoneNumbers[0].number}&contact_exist=true`,
-        )
-        .then(json => {
-          if (
-            json.data == 'User Has Not Registered!'
-            //  ||
-            // (json.data !== null &&
-            //   json.data !== false &&
-            //   json.data !== undefined)
-          ) {
-            contacts[i].isUser = false;
-            // final.push(contacts[i]);
-          } else {
-            contacts[i].isUser = json.data;
-            // final.push(contacts[i]);
-            console.log('push', json.data, i );
-          }
-        })
-        .catch(err => console.log(err.message));
+function format_number(number) {
+  let num = ``;
 
-      // let json = axios.get(
-      //   `http://${API_URL}:8000/user/login?contact_no=${v.phoneNumbers
-      //     .length !== 0 && v.phoneNumbers[0].number}&contact_exist=true`,
-      // );
-      // if (
-      //   json.data == 'User Has Not Registered!' ||
-      //   (json.data !== null &&
-      //     json.data !== false &&
-      //     json.data !== undefined)
-      // ) {
-      //   contacts[i].isUser = false;
-      //   final.push(contacts[i]);
-      // } else {
-      //   consol.log("push", json.data)
-      //   contacts[i].isUser = json.data;
-      //   final.push(contacts[i]);
-      // }
-    });
-    // if (final.length === contacts) {
-    // console.log('hi');
-    return contacts.sort((a, b) => a.isUser - b.isUser);
-    // }
+  let splitNumberDash = number.split('-');
+  splitNumberDash.map(v => {
+    num += v;
+  });
+  let splitNumberPlusZero = num.split('+0');
+  num = ``;
+  splitNumberPlusZero.map(v => {
+    num += v;
+  });
+  let splitNumberPlus = num.split('+');
+
+  num = ``;
+  splitNumberPlus.map(v => {
+    num += v;
+  });
+  let splitNumberPlus92 = num.split('+92');
+
+  num = ``;
+  splitNumberPlus92.map(v => {
+    num += v;
+  });
+  let splitNumberSpace = num.split(' ');
+
+  number = `+92`;
+  splitNumberSpace.map(v => {
+    number += v;
+  });
+  var lastFive = '+92' + number.substr(number.length - 10); // => "Tabs1"
+
+  return lastFive;
+}
+
+function uniq_fast(a) {
+  var seen = {};
+  var out = [];
+  var len = a.length;
+  var j = 0;
+  for (var i = 0; i < len; i++) {
+    var item = a[i];
+    if (seen[item.phoneNumbers] !== 1) {
+      seen[item.phoneNumbers] = 1;
+      out[j++] = item;
+    }
   }
+  return out;
+}
+function Home({navigation}) {
+  const [allMobileUsers, setAllMobileUsers] = useState({
+    arr_exists: [],
+    arr_not_exists: [],
+  });
+  const [loading, setLoading] = useState(false);
+  console.log('contact list');
+  function fetchData(contacts) {}
   useEffect(() => {
     PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
       title: 'Contacts',
       message: 'This app would like to view your contacts.',
       buttonPositive: 'Please accept bare mortal',
     }).then(e => {
+      console.log(format_number('0334 3762430'));
       try {
         Contacts.getAll((err, contacts) => {
           if (err === 'denied') {
           } else {
+            let request_arr = [];
             // setLoading(true);
 
-            // if (fetchData(contacts).length === contacts.length) {
-              setAllMobileUsers(fetchData(contacts));
-              // setLoading(false);
-            // }
+            contacts.map(v => {
+              if (v.phoneNumbers[0]) {
+                let contact = {
+                  phoneNumbers: format_number(v.phoneNumbers[0].number),
+                  name: v.displayName,
+                  raw_no: v.phoneNumbers[0].number,
+                };
+                request_arr.push(contact);
+              }
+            });
+            // console.log(uniq_fast(request_arr), request_arr.length, uniq_fast(request_arr).length)
+
+            axios
+              .post('http://' + API_URL + ':8000/user/if_contact_is_user', {
+                contacts: uniq_fast(request_arr),
+              })
+              .then(contacts => {
+                console.log(Object.keys(contacts.data));
+                setAllMobileUsers(contacts.data);
+              });
           }
         });
       } catch (err) {
@@ -102,62 +124,28 @@ console.log("contact list")
       callIcon = 'https://img.icons8.com/color/48/000000/video-call.png';
     }
 
-    // React.useEffect(() => {
-
-    // });
-
-    if (
-      item.isUser !== null &&
-      item.isUser !== false &&
-      item.isUser !== undefined
-    ) {
-      // console.log(
-      //   item.isUser !== null,
-      //   item.isUser !== false,
-      //   item.displayName,
-      //   item.isUser,
-      // );
-      return (
-        <TouchableOpacity onPress={() => navigation.navigate('ChatRoom')}>
-          <View style={styles.row}>
-            <View style={styles.avatarBackground}>
-              <Image source={{uri: defaultImage}} style={styles.pic} />
+    return (
+      <TouchableOpacity
+        onPress={() =>
+          item._id
+            ? navigation.navigate('ChatRoom')
+            : alert('User is not part of the app')
+        }>
+        <View style={styles.row}>
+          <View style={[styles.avatarBackground, {borderColor: item._id ? "blue": "orange"}]}>
+            <Image source={{uri: defaultImage}} style={styles.pic} />
+          </View>
+          <View>
+            <View style={styles.nameContainer}>
+              <Text style={styles.nameTxt}>{item.name}</Text>
             </View>
-            <View>
-              <View style={styles.nameContainer}>
-                <Text style={styles.nameTxt}>{item.displayName}</Text>
-                {/* <View style={styles.activeCircle} /> */}
-              </View>
-              <View style={styles.end}>
-                <Text style={styles.nameTxt}>
-                  {item.phoneNumbers.length !== 0 &&
-                    item.phoneNumbers[0].number}{' '}
-                  {item.isUser !== null && item.isUser === false
-                    ? 'invite'
-                    : ''}
-                </Text>
-              </View>
-              {/* <View style={styles.end}>
-              <Image
-                style={[
-                  styles.icon,
-                  {marginLeft: 15, marginRight: 5, width: 14, height: 14},
-                ]}
-                source={{
-                  uri: 'https://img.icons8.com/small/14/000000/double-tick.png',
-                }}
-              />
-              <Text style={styles.time}>
-                {item.date} {item.time}
-              </Text>
-            </View> */}
+            <View style={styles.end}>
+              <Text style={styles.nameTxt}>{item.raw_no}</Text>
             </View>
           </View>
-        </TouchableOpacity>
-      );
-    } else {
-      return <View />;
-    }
+        </View>
+      </TouchableOpacity>
+    );
   }
 
   return (
@@ -165,16 +153,30 @@ console.log("contact list")
       navigation={navigation}
       screen="New Chat"
       tab="Home">
-      <View style={{flex: 1}}>
+      <View>
         {/* <Text>{loading + 'desu' + allMobileUsers.length}</Text> */}
         <FlatList
-          extraData={allMobileUsers}
-          data={allMobileUsers}
+          extraData={[
+            ...allMobileUsers.arr_exists,
+            ...allMobileUsers.arr_not_exists,
+          ]}
+          data={[
+            ...allMobileUsers.arr_exists,
+            ...allMobileUsers.arr_not_exists,
+          ]}
           keyExtractor={item => {
-            return item.recordID;
+            return item._id;
           }}
           renderItem={renderItem}
         />
+        {/* <FlatList
+          extraData={allMobileUsers.arr_not_exists}
+          data={allMobileUsers.arr_not_exists}
+          keyExtractor={item => {
+            return item._id;
+          }}
+          renderItem={renderItem}
+        /> */}
       </View>
     </CustomComponents.Footer>
   );
